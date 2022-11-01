1d2743b70359b9b8b0cdd6161a3b3667ce08bdac
HollyW27 <111414391+HollyW27@users.noreply.github.com>
2022-11-01 17:35:57 +0000

    Feature: Help message

--- SRC/functions/handler/handleCommands.js
+++ SRC/functions/handler/handleCommands.js
@@ -1,10 +1,12 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const fs = require('fs');
+const{ SlashCommandBuilder } = require('discord.js');
 
 module.exports =(client) =>{
     client.handleCommands = async()=> {
     const commandFolders = fs.readdirSync("./SRC/commands");
+        const helpDetails = new Array();
     for (const folder of commandFolders){
         const commandFiles = fs
         .readdirSync(`./SRC/commands/${folder}`)
@@ -14,9 +16,25 @@ module.exports =(client) =>{
         for (const file of commandFiles){
             const command = require(`../../commands/${folder}/${file}`)
            commands.set(command.data.name, command);
-           commandArray.push(command.data.toJSON()); 
+           commandArray.push(command.data.toJSON());
+           helpDetails.push(`> **/${command.data.name}**\n > ${command.data.description}\n `)
         }
     }
+    help = { data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Tells you all the commands'),
+    async execute(interaction, client){
+        const message = await interaction.deferReply({
+            fetchReply: true
+        })
+
+        const newMessage = `These are all of the commands\n` + helpDetails.join("");
+        await interaction.editReply({
+            content: newMessage
+        });
+    }}
+    client.commands.set(help.data.name, help)
+    client.commandArray.push(help.data.toJSON())
 
     const clientId = '1036626541687353424';
     const guildId = '1036628022033395802';
